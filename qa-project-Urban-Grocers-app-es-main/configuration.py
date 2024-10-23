5138ff33242192823c32fadbbbd85d3f9a7b5e0e
Lachy-QA <cervettolazaro92@gmail.com>
2024-10-23 17:40:59 -0300

    Commit del proyecto Urban Grocers

---
+++ qa-project-Urban-Grocers-app-es-main/configuration.py
@@ -0,0 +1,8 @@
+# Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = 'https://cnt-30efbc63-c92d-4ba7-b661-38205ea2b9b2.containerhub.tripleten-services.com'
+
+# Almacena la ruta para crear un usuario o usuaria en esta variable
+CREATE_USER_PATH = '/api/v1/users'
+
+# Almacena la ruta para crear un kit en esta variable
+KITS_PATH = '/api/v1/kits'
---
+++ qa-project-Urban-Grocers-app-es-main/data.py
@@ -0,0 +1,13 @@
+# Write the bodies (JSON format);
+# Include the name body and headers as well
+
+# Headers for request POST of Create a new user
+headers_create_new_user = {"Content-Type": "application/json"}
+
+# Body for request POST of Create a new user
+body_create_new_user_minimal_data = {"firstName": "Max",
+    "phone": "+10005553535",
+    "address": "8042 Lancaster Ave.Hamburg, NY"}
+
+# Headers for request POST of Create a personal kit
+headers_create_personal_kit = {"Content-Type": "application/json"}
