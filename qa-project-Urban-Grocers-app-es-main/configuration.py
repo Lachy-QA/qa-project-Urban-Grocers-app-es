44177b2b48a2298e4e55e66bc81b935f4cfaaa0e
Lachy-QA <cervettolazaro92@gmail.com>
2024-10-26 16:42:05 -0300

    Proyecto #7

--- qa-project-Urban-Grocers-app-es-main/configuration.py
+++ qa-project-Urban-Grocers-app-es-main/configuration.py
@@ -1,5 +1,5 @@
 # Inserta tu dirección de URL sin la barra diagonal al final
-URL_SERVICE = 'https://cnt-30efbc63-c92d-4ba7-b661-38205ea2b9b2.containerhub.tripleten-services.com'
+URL_SERVICE = 'https://cnt-737acdb4-d2df-4655-8229-63d3aa171bce.containerhub.tripleten-services.com'
 
 # Almacena la ruta para crear un usuario o usuaria en esta variable
 CREATE_USER_PATH = '/api/v1/users'
