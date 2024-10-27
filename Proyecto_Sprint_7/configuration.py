b74304e65159238fa01dd1ff11da4f640b240c1a
Lachy-QA <cervettolazaro92@gmail.com>
2024-10-27 10:56:27 -0300

    Sprint 7

--- Proyecto_Sprint_7/configuration.py
+++ Proyecto_Sprint_7/configuration.py
@@ -1,5 +1,5 @@
 # Inserta tu dirección de URL sin la barra diagonal al final
-URL_SERVICE = 'https://cnt-74a2eb0f-fc84-43fd-9da9-aca4d5eb97e1.containerhub.tripleten-services.com'
+URL_SERVICE = 'https://cnt-ab1dd4a5-6eab-4602-824d-8f3cc4e645bc.containerhub.tripleten-services.com'
 
 # Almacena la ruta para crear un usuario o usuaria en esta variable
 CREATE_USER_PATH = '/api/v1/users'
